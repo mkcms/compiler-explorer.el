228f562f0a7aa5edbb56ed8da6b6844fd013436e
Michał Krzywkowski <k.michal@zoho.com>
2025-02-24 13:32:02 +0100

    Update copyright years
    * README.md:
    * compiler-explorer-test.el:
    * compiler-explorer.el: Update copyright years.

--- compiler-explorer-test.el
+++ compiler-explorer-test.el
@@ -1,6 +1,6 @@
 ;;; compiler-explorer-test.el --- Tests for compiler-explorer  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2020-2024  Michał Krzywkowski
+;; Copyright (C) 2020-2025  Michał Krzywkowski
 
 ;; Author: Michał Krzywkowski <k.michal@zoho.com>
 ;; Keywords: tests
